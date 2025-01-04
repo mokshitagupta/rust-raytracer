f91e9cb17d2265934dea32dafda3d35d86f20ab8
mokshiz <mokshitacollege@gmail.com>
2025-01-03 21:06:15 -0800

    gamma correction

--- src/color.rs
+++ src/color.rs
@@ -1,11 +1,26 @@
 use crate::intervals::Interval;
 pub use crate::vec3::Vec3 as Color3;
 
+#[inline(always)]
+pub fn lin2gamma(p: f64) -> f64 {
+    if p > 0.0 {
+        return p.sqrt();
+    } else {
+        return 0.0;
+    }
+}
+
 pub fn write_clr(pixel: Color3, stderr: bool) {
     let intensity: Interval = Interval::from(0.0, 0.999);
-    let r = (intensity.clamp(pixel.x()) * (255.0 as f64));
-    let g = (intensity.clamp(pixel.y()) * (255.0 as f64));
-    let b = (intensity.clamp(pixel.z()) * (255.0 as f64));
+    let mut r = pixel.x();
+    let mut g = pixel.y();
+    let mut b = pixel.z();
+    r = lin2gamma(r);
+    g = lin2gamma(g);
+    b = lin2gamma(b);
+    r = (intensity.clamp(r) * (255.0 as f64));
+    g = (intensity.clamp(g) * (255.0 as f64));
+    b = (intensity.clamp(b) * (255.0 as f64));
 
     if stderr {
         eprintln!("{r} {g} {b}");
